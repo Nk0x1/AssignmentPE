56aac8ffa912e31b0fa8d9b707ecdb16d94573a5
Nk0x1 <114907090+Nk0x1@users.noreply.github.com>
2024-09-06 00:31:42 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+
+document.getElementById('welcomebtn').addEventListener('click', function() {
+    alert('Hello, Welcome to my page!');
+});
+document.getElementById('profilepic').addEventListener('mouseover', function() {
+    document.body.style.backgroundColor = '#00FFFF';
+});
+document.getElementById('profilepic').addEventListener('mouseout', function() {
+    document.body.style.backgroundColor = '	#D8BFD8';
+});
